944e4e55a34c641679cbd368a78db48b431d5637
Ulises Santana <ulisesantana@gmail.com>
2022-12-11 11:12:25 +0000

    WIP: day 10 part 2

--- src/day-10/solution.ts
+++ src/day-10/solution.ts
@@ -5,6 +5,9 @@ import { Task } from './Task'
 class VideoSystemReplacement {
   private X = 1
   private readonly signalStrengths = [] as number[]
+  private readonly screenWidth = 40
+  private screen = [] as string[]
+  private screenLines = 0
   run (commands: string[]) {
     this.executeCycle(commands.map(Task.fromCommand))
     return this
@@ -12,6 +15,17 @@ class VideoSystemReplacement {
 
   getSignalStrengthForCycles (...cycles: number[]) {
     return this.signalStrengths.filter((_, index) => cycles.includes(index + 1))
+  }
+
+  getScreenShot () {
+    return [
+      this.screen.slice(0, 40),
+      this.screen.slice(40, 80),
+      this.screen.slice(80, 120),
+      this.screen.slice(120, 160),
+      this.screen.slice(160, 200),
+      this.screen.slice(200, 240)
+    ].map(line => line.join('')).join(EOL)
   }
 
   private executeCycle ([task, ...tasks]: Task[]) {
@@ -22,16 +36,40 @@ class VideoSystemReplacement {
       tasks = tasks.slice(1)
     }
     const currentCycle = this.signalStrengths.length + 1
-    this.signalStrengths.push(this.X * currentCycle)
+    this.addSignalStrength(currentCycle)
+    this.drawPixel(currentCycle)
     task.finishCycle()
     return this.executeCycle([task, ...tasks])
+  }
+
+  private addSignalStrength (currentCycle: number) {
+    this.signalStrengths.push(this.X * currentCycle)
+  }
+
+  private drawPixel (currentCycle: number) {
+    const ctrPositions = this.getCRTPositions(currentCycle)
+    if (ctrPositions.includes(this.X)) {
+      this.screen.push('#')
+    } else {
+      this.screen.push('.')
+    }
+    if ((this.screen.length / this.screenWidth) > (this.screenLines + 1)) {
+      this.screenLines += 1
+    }
+  }
+
+  private getCRTPositions (currentCycle: number) {
+    const centerPosition = currentCycle - (this.screenWidth * this.screenLines)
+    return [centerPosition - 2, centerPosition - 1, centerPosition]
   }
 }
 
 export function sumSignalStrengths (input: string) {
-  const vsr = new VideoSystemReplacement()
-  return sum(
-    ...vsr.run(split(EOL, input))
-      .getSignalStrengthForCycles(20, 60, 100, 140, 180, 220)
-  )
+  const vsr = new VideoSystemReplacement().run(split(EOL, input))
+  return sum(...vsr.getSignalStrengthForCycles(20, 60, 100, 140, 180, 220))
+}
+
+export function print (input: string) {
+  const vsr = new VideoSystemReplacement().run(split(EOL, input))
+  return vsr.getScreenShot()
 }
--- src/day-10/test.ts
+++ src/day-10/test.ts
@@ -1,10 +1,13 @@
-import { sumSignalStrengths } from './solution'
-import { expectedOutput, fixture } from './fixture'
+import { print, sumSignalStrengths } from './solution'
+import { expectedOutputPart1, expectedOutputPart2, fixture } from './fixture'
 import { readFile } from '../utils'
 
 describe('Day 10', () => {
-  it('should sum the signal strengths based on cycles', async () => {
-    expect(sumSignalStrengths(fixture)).toBe(expectedOutput)
+  xit('should sum the signal strengths based on cycles', async () => {
+    expect(sumSignalStrengths(fixture)).toBe(expectedOutputPart1)
     expect(sumSignalStrengths(await readFile('src/day-10/input.txt'))).toBe(12880)
+  })
+  it('should render something', () => {
+    expect(print(fixture)).toBe(expectedOutputPart2)
   })
 })
